a9a1811c7be6abbab94eb5e89eff2eb4722f2e9e
Mahwou <ndimbucourage@gmail.com>
2026-06-04 14:59:46 +0100

    update (APP): add row dragged item to dragged items.

--- src/components/DragItem.ts
+++ src/components/DragItem.ts
@@ -4,11 +4,4 @@ export type ColumnDragItem = {
     type: "COLUMN"
 }
 
-export type RowDragItem = {
-    id: string,
-    text: string,
-    type: "ROW"
-}
-
-
-export type DragItem = ColumnDragItem | RowDragItem
+export type DragItem = ColumnDragItem
